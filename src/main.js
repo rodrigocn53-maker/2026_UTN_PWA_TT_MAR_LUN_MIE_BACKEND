9fc1d652fd915b0701e9dbb0e143773691e50ca0
rodrigocn53-maker <rodrigocn53@gmail.com>
2026-04-08 20:02:16 -0300

    arreglo-env

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import ENVIRONMENT from "./config/environment.config.js"
 import connectMongoDB from "./config/mongoDB.config.js"
 /* import User from "./models/user.model.js"
 import Workspace from "./models/workspace.model.js" */
-import WorkspaceMember from "./models/workspaceMember.model.js"
+/* import WorkspaceMember from "./models/workspaceMember.model.js"
 import workspaceMemberRepository from "./repository/member.repository.js"
 import userRepository from "./repository/user.repository.js"
-import workspaceRepository from "./repository/workspace.repository.js"
+import workspaceRepository from "./repository/workspace.repository.js" */
 import express, { response } from 'express';
 import healthRouter from "./routes/health.router.js"
 import authRouter from "./routes/auth.router.js"
-import mailerTransporter from "./config/mailer.config.js"
+/* import mailerTransporter from "./config/mailer.config.js" */
 import cors from 'cors'
 import authMiddleware from "./middlewares/authMiddleware.js"
 import workspaceRouter from "./routes/workspace.router.js"
